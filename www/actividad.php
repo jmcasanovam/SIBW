d0e633f3c40eadd88807df969d748b8b0a30aff8
jmcasanovam <6jmcasanovam@gmail.com>
2024-04-25 17:29:44 +0200

    comienzo con el envio de variables en actividad.html

--- www/actividad.php
+++ www/actividad.php
@@ -18,5 +18,13 @@
   
   
   // echo $twig->render('evento.html', ['nombre' => $nombreEvento, 'fecha' => $fechaEvento]);
-  echo $twig->render('actividad.html', []);
+
+  $argumentos = [];
+  $argumentos['nombre'] = "Escalada";
+  $argumentos['fecha'] = "03/06/2024";
+  $argumentos['precio'] = "59.99€";
+  $argumentos['contenido'] = "La escalada es una actividad deportiva que consiste en subir por paredes de roca, montañas o estructuras artificiales. Se puede practicar en interiores o exteriores. La escalada en roca se realiza en paredes naturales, mientras que la escalada en hielo se realiza en cascadas de hielo. La escalada en bloque se realiza en bloques de roca de pequeño tamaño. La escalada deportiva se realiza en paredes artificiales y la escalada en solitario se realiza sin compañero de cuerda. La escalada en grandes paredes se realiza en paredes de gran altura y la escalada en psicobloc se realiza sobre el agua.";
+  // $argumentos['imagen1'] = "img/escalada.jpg";
+  // $argumentos['imagen2'] = "img/escalada2.jpg";
+  echo $twig->render('actividad.html', $argumentos);
 ?>
