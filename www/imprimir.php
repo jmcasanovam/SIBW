3547c8f56c3645c0eaa1776b0413047961061126
jmcasanovam <6jmcasanovam@gmail.com>
2024-04-26 15:52:40 +0200

    separo las funciones de cargar información para poder usarlas en imprimir

--- www/imprimir.php
+++ www/imprimir.php
@@ -1,9 +1,12 @@
 <?php
     require_once "/usr/local/lib/php/vendor/autoload.php";
+    require_once "./cargar_informacion.php";
 
     $loader = new \Twig\Loader\FilesystemLoader('templates');
     $twig = new \Twig\Environment($loader);
 
-    echo $twig->render('imprimir.html',[]);
+    $argumentos = gestionarParametros();
+
+    echo $twig->render('imprimir.html', $argumentos);
     
 ?>
